b597db018222eacef16158d60ca40c5c403122f6
Nam Bui <bnnam@ymail.com>
2019-11-18 11:25:57 +0100

    add lines to pdf by coding

--- src/cvexport.js
+++ src/cvexport.js
@@ -4,14 +4,18 @@ import * as jsPDF from 'jspdf'
 //object to create PDF
 let doc = new jsPDF()
 let textOut = ''
-
+let i=1
 const cvExport = (exportList) => {
   console.log('exportList :', exportList)
+  //doc.text(text,column, row)
   exportList.map((item) => {
-    textOut += 'Name:' + item.name + '   Age:' + item.age + '\r\n'
+    doc.text('Name:' + item.name + '   Age:' + item.age + '\r\n', 10, 10*i)
+    i=i+1
+    // doc.addImage('./src/employee.jpg','JPEG', 10, 10*i,20,20)
+    // i=i+4
   })
-    console.log(textOut)
-    doc.text(textOut,10,10)
+    //console.log(textOut)
+    //doc.text(textOut,10,10)
     doc.save('cvListExport.pdf')
 }
 
