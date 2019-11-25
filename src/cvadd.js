6c15699ff8ac1f6b426a7bd7c91f0693663a9212
Nam Bui <bnnam@ymail.com>
2019-11-25 14:20:21 +0100

    Fixing hook problem

--- src/cvadd.js
+++ src/cvadd.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react'
 
 // to add new CVs
-const AddCV = () =>{
-
-}
 const CvAdd = () => {
+  const [persons, setPersons] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newAge, setNewAge] = useState('')
+
+
+  const AddCV = ()=> {}
+
+  // const AddCV = (event) => {
+  //   event.preventDefault()
+  //   console.log('button clicked', event.target)
+  //   const person = { name: newName, number: newAge }
+  //   if (listOfCV.some(personCheck => personCheck.name === person.name)) {
+  //     const warning = newName + ' is alreardy in the CV database'
+  //     window.alert(warning)
+  //     setNewName('')
+  //     setNewAge('')
+  //   } else {
+  //     setPersons(persons.concat(person))
+  //     setNewName('')
+  //     setNewAge('')
+  //   }
+  // }
+
+  // }
+
+
+  const handleNameChange = (event) => {
+    console.log('Target: ', event.target.value)
+    setNewName(event.target.value)
+  }
+  const handleAgeChange = (event) => {
+    console.log('Target: ', event.target.value)
+    setNewAge(event.target.value)
+  }
   return (
     <div>
-      <form >
+      <form>
         <div>
-          Name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          Age: <input />
-        </div>
+          number: <input value={newAge} onChange={handleAgeChange} /></div>
         <div>
-          <button onClick={() => AddCV()}>Add</button></div>
+          <button type="submit">add</button>
+        </div>
       </form>
     </div>
   );
