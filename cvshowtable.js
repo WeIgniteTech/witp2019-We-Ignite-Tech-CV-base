9713ae56607494a1c0034039f0978d098cd0c4fa
Nam Bui <bnnam@ymail.com>
2019-12-01 13:16:56 +0100

    test airtable

---
+++ cvshowtable.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//retrieve table from airtable database
+
+// https://flaviocopes.com/airtable/
+// https://airtable.com/appHifVOL5knsbeGc/api/docs#curl/table:cv-base
+// https://github.com/Airtable/airtable.js
+
+const CvShowTable = () => {
+
+}
+
+export default CvShowTable;
