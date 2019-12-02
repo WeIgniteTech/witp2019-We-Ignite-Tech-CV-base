42f3415c611f20795cce504188df86cae4c5994d
Nam Bui <bnnam@ymail.com>
2019-12-02 11:55:33 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -11,8 +11,10 @@ import CvShowData from './CvShowData';
 
 
 
-const listOfCV = CvListData();
+const listOfCV = CvListData()
+
 console.log('list of CV app: ', listOfCV);
+console.log('list of CV app 0: ', listOfCV[0]);
 const introductionText="The We Ignite Tech program is a three month evening course aiming to get people with a tech background closer to the norwegian IT market. The first iteration of the We Ignite Tech program has just finished. Twelve participants just graduated and some of them have already steped into the IT market. Enlighted by this positive experience, we will start a new session of the program in september 2019. A Step Closer To Your IT Career In Norway. \n ACCURATE TECHNOLOGIES: The program is based on modern and well used technologies (programming languages, frameworks, tools and methodologies). INDUSTRY STANDARDS: Course material is based on our teachers experience from the field in the norwegian and international IT industry. The We Ignite Tech program is a three month evening course aiming to get people with a tech background closer to the norwegian IT market. The first iteration of the We Ignite Tech program has just finished. Twelve participants just graduated and some of them have already steped into the IT market.  Enlighted by this positive experience, we will start a new session of the program in september 2019. \r\n A Step Closer To Your IT Career In Norway. ACCURATE TECHNOLOGIES: The program is based on modern and well used technologies (programming languages, frameworks, tools and methodologies). \n\ INDUSTRY STANDARDS: Course material is based on our teachers experience from the field in the norwegian and international IT industry."
 function App() {
     return (
--- src/CvListData.js
+++ src/CvListData.js
@@ -8,23 +8,26 @@ import React from 'react';
 //https://flaviocopes.com/airtable/
 
 
-let dataCV = new Array();
 
+let dataCV = [];
 const CvListData = () => {
+
     const fetch = require('node-fetch');
     const urlCV = 'https://api.airtable.com/v0/appHifVOL5knsbeGc/CV-base?api_key=keymR4xBC0cAWCtQX'
 
-    fetch(urlCV).then(res => res.json()).then((json) => {
-        json.records.map((item, i) => {
+    fetch(urlCV).then(res => res.json()).then((data) => {
+        data.records.map((item, i) => {
             dataCV.push(item.fields)
         })
         console.log('Data CV in fetch:', dataCV)
         //dataCV = json.records
-        // console.log('Data json.records:', json.records)
+        console.log('Data CV[0] in fetch:', dataCV[0])
     })
-    console.log('Data out fetch:', dataCV)
-    console.log('Data out fetch 0:', dataCV[0])
-
+    .catch(error => console.log('Error: ',error))
+    
+    //return an object not an Array
+    console.log('Data CV out fetch:', dataCV)
+    console.log('Data CV[0] out fetch:', dataCV[0])
     return dataCV
 }
 
